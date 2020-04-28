c64ff668c996788ca2df398f99756283ced70ae0
Ruben Baskaran <rbaskaran92@gmail.com>
2020-04-28 22:23:30 +0200

    added mongodb

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const config = require("./config");
+
+mongoose
+  .connect(
+    "mongodb+srv://rubenbaskaran:" +
+      config.development.database.password +
+      "@reacttutorialmongodb-hkyfr.gcp.mongodb.net/test?retryWrites=true&w=majority",
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("DB connected"))
+  .catch((err) => console.error(err));
 
 app.get("/", (req, res) => {
   res.send("hello world");
