bda7dc41c352c5e50c866974a917cc09465bf0d8
Ruben Baskaran <rbaskaran92@gmail.com>
2020-05-01 20:22:03 +0200

    added post route for user registration

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const config = require("./config");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const { User } = require("./model/user");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
 
 mongoose
   .connect(
@@ -15,6 +22,16 @@ mongoose
 
 app.get("/", (req, res) => {
   res.send("hello world");
+});
+
+app.post("/api/users/register", (req, res) => {
+  const user = new User(req.body);
+  user.save((err, userData) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
 });
 
 app.listen(5000);
